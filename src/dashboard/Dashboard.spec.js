86d1ce17e303cb5dacb3e213ed1f702370e50737
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-15 17:13:44 -0400

    Created a test for getting a snapshot of the Dashboard component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
 // Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+	it('should run Dashboard tests', () => {
+		expect(true).toBe(true);
+	});
+
+	it('matches snapshot', () => {
+		const tree = renderer.create(<Dashboard />);
+
+		expect(tree.toJSON()).toMatchSnapshot();
+	});
+});
